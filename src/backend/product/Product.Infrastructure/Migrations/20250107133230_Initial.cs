8665ad15ad097b3bf6bc94c57d741cf6b80b3aba
Mohammad Alfi Shahri <mohammad.alfi@day1lab.app>
2025-01-07 20:33:41 +0700

    add initial migrations

---
+++ src/backend/product/Product.Infrastructure/Migrations/20250107133230_Initial.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Product.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            migrationBuilder.CreateTable(
+                name: "Product",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(28,10)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_CreatedAt",
+                schema: "dbo",
+                table: "Product",
+                column: "CreatedAt");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_Name",
+                schema: "dbo",
+                table: "Product",
+                column: "Name");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_Price",
+                schema: "dbo",
+                table: "Product",
+                column: "Price");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product",
+                schema: "dbo");
+        }
+    }
+}
